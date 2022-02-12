cc542f04647cdad996b26e1d1ef29ed5dc9f2b85
doubleblindresearcher <97717878+doubleblindresearcher@users.noreply.github.com>
2022-02-11 23:59:39 -0500

    Added generated configs directory for run_paper_experiments.py

--- run_paper_experiments.py
+++ run_paper_experiments.py
@@ -3,9 +3,9 @@ from sumie.utils.experiment import exps_from_config_diffs
 print('=======================================')
 print('Running sequence labeling experiments!')
 print('=======================================')
-exps_from_config_diffs('configs/paper_sequence_labeling_configs.json', generated_configs_dir=None, run_exps=True)
+exps_from_config_diffs('configs/paper_sequence_labeling_configs.json', generated_configs_dir='generated_configs/', run_exps=True)
 
 print('=======================================')
 print('Running baseline experiments!')
 print('=======================================')
-exps_from_config_diffs('configs/paper_baseline_configs.json', generated_configs_dir=None, run_exps=True)
+exps_from_config_diffs('configs/paper_baseline_configs.json', generated_configs_dir='generated_configs/', run_exps=True)
